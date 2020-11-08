e06e4ce9ee0a5df661ebb2d4bcde6fdc0753b3b0
Ramon-Goveia <ramonalangoveia@hotmail.com>
2020-11-08 14:32:21 -0300

    Implementação da rota completa com os metodos GET, POST, PUT, DELETE

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -17,12 +17,43 @@ const getUsers = () => {
 
 const saveUser = (users) => fs.writeFileSync(filePath, JSON.stringify(users, null, '\t'))
 
-const userRoute = (app) => {
+function userRoute(app) {
     app.route('/users/:id?')
         .get((req, res) => {
             const users = getUsers()
 
             res.send({ users })
+        })
+        .post((req, res) => {
+            const users = getUsers()
+
+            users.push(req.body)
+            saveUser(users)
+
+            res.status(201).send('OK')
+        })
+        .put((req, res) => {
+            const users = getUsers()
+
+            saveUser(users.map(user => {
+                if (user.id === req.params.id) {
+                    return {
+                        ...user,
+                        ...req.body
+                    }
+                }
+
+                return user
+            }))
+
+            res.status(200).send('oK')
+        })
+        .delete((req, res) => {
+            const users = getUsers()
+
+            saveUser(users.filter(user => user.id !== req.params.id))
+
+            res.status(200).send('OK')
         })
 }
 
